92f79b3d98b857d6e8c085a03be731ace7ca6862
Cristian Romero <cristianrch123@gmail.com>
2020-12-10 21:32:41 -0500

    first commit

---
+++ db/employee_db.py
@@ -0,0 +1,32 @@
+from typing import  Dict
+from pydantic import BaseModel
+from typing import Optional
+
+class EmployeeInDB(BaseModel):
+    username: str
+    password: str
+    rol: str
+    task: Optional[str] = None
+    logged_in: bool
+
+database_employees = Dict[str, EmployeeInDB]
+
+database_employees = {
+    "empleado1": EmployeeInDB(**{"username":"empleado1",
+                            "password":"123456",
+                            "rol":"operator",
+                            "logged_in": False}),
+}
+
+def get_employee(username: str):
+    if username in database_employees.keys():
+        return database_employees[username]
+    else:
+        return None
+
+def update_employee(employee_in_db: EmployeeInDB):
+    database_employees[employee_in_db.username] = employee_in_db
+    return employee_in_db
+
+
+
---
+++ main.py
@@ -0,0 +1,61 @@
+from db.employee_db import EmployeeInDB
+from db.employee_db import update_employee, get_employee
+from models.employee_models import EmployeeLogin, EmployeeLogout, EmployeeTask
+from fastapi import FastAPI, HTTPException
+
+api = FastAPI(
+    title="Sprint 1",
+    description="APIs para el modulo de empleado",
+    version="0.0.1",
+    )
+
+@api.post("/employee/auth/")
+async def auth_employee(employee_login: EmployeeLogin):
+
+    employee_in_db = get_employee(employee_login.username)
+
+    if employee_in_db == None:
+        raise HTTPException(status_code=404, detail="El empleado no existe")
+
+    if employee_in_db.password != employee_login.password:
+        return  {"Autenticado": False}
+    else:
+        employee_in_db.logged_in = True
+        update_employee(employee_in_db)
+        return  {"Autenticado": True}
+
+
+@api.get("/employee/data/{username}")
+async def get_employee_data(username: str):
+
+    employee_in_db = get_employee(username)
+
+    if employee_in_db == None:
+        raise HTTPException(status_code=404, detail="El usuario no existe")
+
+    return  employee_in_db
+
+
+@api.get("/employee/signout/{username}")
+async def signout_employee(username: str):
+
+    employee_in_db = get_employee(username)
+    employee_in_db.logged_in = False
+
+    update_employee(employee_in_db)
+
+    return  {"Cerrar Sesión": True}
+
+
+@api.put("/employee/task/")
+async def assign_task(employee_task: EmployeeTask):
+
+    employee_in_db = get_employee(employee_task.username)
+
+    if employee_in_db == None:
+        raise HTTPException(status_code=404, detail="El usuario no existe")
+
+    employee_in_db.task = employee_task.task
+    update_employee(employee_in_db)
+
+    return  employee_in_db
